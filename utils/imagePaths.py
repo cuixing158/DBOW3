5fb99998bb5a43673a0b650ba9db6732aea401fc
cuixing158 <1229261804@qq.com>
2022-12-28 17:30:55 +0800

    first commit

---
+++ utils/imagePaths.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@file        :imagePaths.py
+@description :生成从小到大的顺序图片
+@date        :2022/12/28 15:57:05
+@author      :cuixingxing
+@email       :cuixingxing150@gmail.com
+@version     :1.0
+'''
+
+
+
+import os
+import functools
+
+# 自定义排序规则
+def my_compare(path1, path2):
+    name1 = os.path.basename(path1)
+    name2 = os.path.basename(path2)
+    if int(name1[:-4]) < int(name2[:-4]):
+        return -1
+    else:
+        return 1
+
+
+imagesFileName = []
+imagesDir = "/opt_disk2/rd22946/my_data/map/3/bride"
+
+for (dirpath, dirnames, filenames) in os.walk(imagesDir):
+    for filename in filenames:
+        if filename.endswith(".jpg"):
+            imagesFileName.append(os.path.join(dirpath, filename))
+
+imagesFileName.sort(key=functools.cmp_to_key(my_compare))
+# imagesFileName = sorted(imagesFileName, key=functools.cmp_to_key(my_compare))
+with open("imagePath.txt", "w") as fid:
+    fid.writelines(name + "\n" for name in imagesFileName)

12816e1802e6d3cda3e242c28c517c0e44f034f5
cuixing158 <1229261804@qq.com>
2023-02-20 16:42:20 +0800

    feat:add opencvAPI

---
+++ c_file/opencvAPI.h
@@ -0,0 +1,32 @@
+/**
+* @file        :opencvAPI.h
+* @brief       :用于在各个嵌入式平台/x86-64上为生成的C/C++代码直接使用OpenCV代码
+* @details     :This is the detail description.
+* @date        :2023/02/20 16:23:24
+* @author      :cuixingxing(cuixingxing150@gmail.com)
+* @version     :1.0
+*
+* @copyright Copyright (c) 2023
+*
+*/
+
+#ifndef _OPENCVAPI_ALLPLATFORM_
+#define _OPENCVAPI_ALLPLATFORM_
+
+// base
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+// OpenCV
+#include "opencv2/opencv.hpp"
+
+typedef struct imref2d {
+    double XWorldLimits[2];
+    double YWorldLimits[2];
+    uint32_t ImageSize[2];
+} imref2d;
+
+extern void imwarp2(const unsigned char *inImg[], int rows, int cols, int channels, double *tformA[9], imref2d *outputView, unsigned char *outImg[]);
+#endif

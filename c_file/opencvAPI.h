73a9bb19b008776c86b75c4db7758ce1c9b334ba
cuixing158 <1229261804@qq.com>
2023-02-21 17:33:01 +0800

    ✨ feat: refine opencv_API

--- c_file/opencvAPI.h
+++ c_file/opencvAPI.h
@@ -25,8 +25,10 @@
 typedef struct imref2d {
     double XWorldLimits[2];
     double YWorldLimits[2];
-    uint32_t ImageSize[2];
-} imref2d;
+    double ImageSize[2];
+} imref2d_;
 
-extern void imwarp2(const unsigned char *inImg[], int rows, int cols, int channels, double *tformA[9], imref2d *outputView, unsigned char *outImg[]);
+void imwarp(const cv::Mat srcImg, int rows, int cols, int channels, float tformA[], imref2d outputView, cv::Mat &outImg);
+
+void imwarp2(const unsigned char inImg[], int rows, int cols, int channels, double tformA[9], imref2d_ *outputView, unsigned char outImg[]);
 #endif
